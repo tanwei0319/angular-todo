87b48eed4937fec481ad9a66a8a33be317554326
Tan Wei <tanwei0319@gmail.com>
2018-05-11 21:14:15 -0700

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,12 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1 class="app-title">
-      Welcome to {{ title }}!
+      {{ title }}
     </h1>
     <app-list-manager></app-list-manager>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My To-Do List App';
+  title = 'My To-do App!';
 }
